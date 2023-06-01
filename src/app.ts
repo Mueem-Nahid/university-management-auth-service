66834898070c7b6eb8a1b1ea85c3e8e375bf499f
Mueem-Nahid <mueem51@gmail.com>
2023-06-01 22:51:21 +0600

    format codebase

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,18 @@
-import express, { Application, Request, Response } from "express";
-import cors from "cors";
+import express, { Application, Request, Response } from 'express'
+import cors from 'cors'
 
-const app: Application = express();
+const app: Application = express()
 
 // application routes
 
-app.use(cors());
+app.use(cors())
 
 // parse data
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.use('/', (req: Request, res: Response) => {
-   res.send("Hey...")
+  res.send('Hey...')
 })
 
 export default app
